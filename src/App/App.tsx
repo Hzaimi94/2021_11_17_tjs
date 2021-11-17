4b9c186ea20a8eac9569161984ad6d32695c0764
Hzaimi94 <hasnaa.zaimi@gmail.com>
2021-11-17 12:02:42 +0100

    Print Hello

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+// Nom de la fonction = nom du composant
+function App() {
+  return (
+    <div className="App">
+        Hey you!
+    </div>
+  );
+}
+
+export default App;
