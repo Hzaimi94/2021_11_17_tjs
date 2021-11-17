574766407047db7d19a7ddfc9c7536190f3bf1a2
Hzaimi94 <hasnaa.zaimi@gmail.com>
2021-11-17 14:13:11 +0100

    Button component creation

---
+++ src/App/components/Button/Button.js
@@ -0,0 +1,12 @@
+// Pour dire que ces fonctions sont des composants
+import React from "react";
+import './Button.css'
+
+/**
+ * Composant button HTML
+ * @param {*} props du composant
+ * @returns button render
+ */
+const Button = (props) => {
+  return <button className="Button">Submit button</button>;
+};
