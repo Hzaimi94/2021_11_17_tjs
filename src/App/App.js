0b2d4850d9eb7259ebb7f6c62ebf4f7d07a31235
Hzaimi94 <hasnaa.zaimi@gmail.com>
2021-11-18 12:25:43 +0100

    Generate meme

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FlexLayout from "./components/FlexLayout/FlexLayout";
+import Header from "./components/Header/Header";
 import MemeForm from "./components/MemeForm/MemeForm";
 import MemeViewer from "./components/MemeViewer/MemeViewer";
 
@@ -21,35 +22,40 @@ class App extends React.Component {
     );
   }
 
+  // C'est obligatoirement 1 composant parent, pas de siblings !!!!
+  // Donc solution encapsuler dans <> </>
   render() {
     return (
-      <div className="App">
-        <FlexLayout>
-        <MemeViewer
-          meme={{
-            titre: "First meme",
-            text: "Stop cheating",
-            x: 370,
-            y: 530,
-            fontSize: 27,
-            fontWeight: "900",
-            underline: true,
-            italic: true,
-            frameX: 0,
-            frameY: 0,
-            color: "red",
-          }}
-          image={{
-            id: 0,
-            url: "img/empty1.jpg",
-            titre: "meme1",
-            h: 778,
-            w: 736,
-          }}
-        ></MemeViewer>
-        <MemeForm></MemeForm>
-        </FlexLayout>
-      </div>
+      <>
+        <Header></Header>
+        <div className="App">
+          <FlexLayout>
+            <MemeViewer
+              meme={{
+                titre: "First meme",
+                text: "Stop cheating",
+                x: 370,
+                y: 530,
+                fontSize: 27,
+                fontWeight: "900",
+                underline: true,
+                italic: true,
+                frameX: 0,
+                frameY: 0,
+                color: "red",
+              }}
+              image={{
+                id: 0,
+                url: "img/empty1.jpg",
+                titre: "meme1",
+                h: 778,
+                w: 736,
+              }}
+            ></MemeViewer>
+            <MemeForm></MemeForm>
+          </FlexLayout>
+        </div>
+      </>
     );
   }
 }
