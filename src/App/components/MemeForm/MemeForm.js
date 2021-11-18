781ff6e854a68c5e9f653cbb94504770d3620573
Hzaimi94 <hasnaa.zaimi@gmail.com>
2021-11-18 10:00:49 +0100

    Gen hooked component

---
+++ src/App/components/MemeForm/MemeForm.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const initialState = {};
+function MemeForm(props) {
+  const [state, setState] = useState(initialState);
+  useEffect(() => {
+    // didMount + didUpdate
+    return () => {
+      // willUnmount
+    };
+  }, [state]);
+  return <div data-testid="MemeForm">memeForm</div>;
+}
+
+MemeForm.propTypes = {};
+
+MemeForm.defaultProps = {};
+
+export default MemeForm;
